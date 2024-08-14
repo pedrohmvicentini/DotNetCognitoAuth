822d827b9c51279be74424cb09b92d49b52b4b0a
pedrohmvicentini <pedro.medeiros.v@gmail.com>
2024-08-13 23:40:33 -0300

    init project test with cognito as authorization

---
+++ DotNetCognitoAuthAPI/Controllers/SecureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotNetCognitoAuthAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        public HttpStatusCode AuthorizeUser()
+        {
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ DotNetCognitoAuthAPI/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var authority = builder.Configuration["Authentication:Cognito:Authority"];
+    var audience = builder.Configuration["Authentication:Cognito:Audience"];
+    var issuer = builder.Configuration["Authentication:Cognito:Issuer"];
+
+    options.Authority = authority;
+    options.Audience = audience;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("{your-secret-key}")) // If using a secret key
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
